20567d4ed131130d485d8f5f15c6f27993204b66
Mushegh Torosyan <torosyanmushegh@gmail.com>
2025-01-03 17:33:41 +0400

    add filemanager

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,7 @@ Route::middleware('auth')->group(function () {
 
 });
 
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
+    \UniSharp\LaravelFilemanager\Lfm::routes();
+});
+
